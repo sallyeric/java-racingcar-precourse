e7cc8176d8b5ed88dc83f56334dd2d10e3cd68d3
sallyeric <sally1eric2@gmail.com>
2022-10-10 12:55:35 +0900

    feat: Car 클래스에 차량 전진 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,8 +2,17 @@ package racingcar.domain;
 
 public class Car {
     private String name;
+    private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public void move(int distance) {
+        this.position += distance;
     }
 }
